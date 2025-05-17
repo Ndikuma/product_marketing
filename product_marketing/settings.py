f736e6358c4bb796025d3ea306926adb4963c844
ndikuma <ii61212456@gmail.com>
2025-05-17 18:27:15 +0200

    update settings

--- product_marketing/settings.py
+++ product_marketing/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-i0cqn3lb*n--rwe1plue&(rk^hag)3vr0!6c7c9in+r_qr0v#%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
