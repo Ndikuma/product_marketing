7168cb31982e0869ba851cc6943d20312d888ea3
ndikuma <ii61212456@gmail.com>
2025-05-17 18:50:41 +0200

    allows all hosts

--- product_marketing/settings.py
+++ product_marketing/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-i0cqn3lb*n--rwe1plue&(rk^hag)3vr0!6c7c9in+r_qr0v#%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
